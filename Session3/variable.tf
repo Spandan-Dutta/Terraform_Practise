e06679099c2ff340c31c5078738bc68357987a51
Spandan Dutta <67653554+Spandan-Dutta@users.noreply.github.com>
2022-09-23 10:49:11 +0530

    Update variable.tf

--- Session3/variable.tf
+++ Session3/variable.tf
@@ -2,14 +2,14 @@ variable "access_key" {
     type = string
     description = "This is the access key for this aws account"
     sensitive = true
-    default = "AKIA5XRQ5SVVTMBTVAH7"
+    default = ""
 }
 
 variable "secret_key" {
     type = string
     description = "This is the secret key for this aws account"
     sensitive = true
-    default = "stPdarFemkxdYV/DraXaWDIs5e0XnXPUZXv/Fmtq"
+    default = ""
 }
 
 variable "region" {
